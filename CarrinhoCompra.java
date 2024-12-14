bed38a18c45fe17ec269908eb581439d615fa076
GabrielRdrigues <gabrielrodriguesmarquesvalim@gmail.com>
2024-12-14 13:01:10 -0300

    Atualização das classes

---
+++ CarrinhoCompra.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+public class CarrinhoCompra{
+    ArrayList<ProdutoEstoque> estoque_carrinho= new ArrayList<ProdutoEstoque>();
+    ArrayList<ProdutoEstoque> itens_do_carrinho = new ArrayList<ProdutoEstoque>();
+    
+    double valor_a_pagar=0;
+
+     public CarrinhoCompra(ArrayList<ProdutoEstoque> estoque){
+        estoque_carrinho=estoque;
+     }
+
+    public void adiciona_item(String n,int qtd){
+        for(int i=0;i<3;i++){
+            ProdutoEstoque auxiliar = estoque_carrinho.get(i);
+            if(n.equals(auxiliar.nome)){
+                ProdutoEstoque ProdutoAuxiliar = new ProdutoEstoque(" ",0,0); // Variável auxiliar
+                ProdutoAuxiliar.nome=n;
+                ProdutoAuxiliar.quantidade=qtd;
+                ProdutoAuxiliar.valor= auxiliar.valor;
+                itens_do_carrinho.add(ProdutoAuxiliar); // Adicionando o ProdutoAuxiliar no carrinho
+            }
+        }
+    }
+
+    /*O Método finalizacompra() retira os itens do carrinho do cliente do estoque da loja  */
+    public void finalizaCompra(){ 
+        int tamanho_carrinho = itens_do_carrinho.size();
+        int tamanho_estoque = estoque_carrinho.size();
+        for(int i=0;i<tamanho_carrinho;i++){
+            for(int j=0;j<tamanho_estoque;j++){
+                if((itens_do_carrinho.get(i)).nome == (estoque_carrinho.get(j)).nome){
+                    (estoque_carrinho.get(j)).quantidade += -(itens_do_carrinho.get(i)).quantidade;
+                }
+            }
+        }
+    }
+
+    // Retorna o valor total das compras
+    public double getValor(){
+        return valor_a_pagar;
+    }
+}
--- EstoqueProdutos.java
+++ EstoqueProdutos.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 
 public class EstoqueProdutos{
+    
     ArrayList<ProdutoEstoque> estoque;
-
+    public EstoqueProdutos(){
+        estoque = new ArrayList<ProdutoEstoque>(); // Inicializando o array de produtos pelo construtor
+    }
     public void adicionaProduto(ProdutoEstoque x){
         estoque.add(x);
     }
