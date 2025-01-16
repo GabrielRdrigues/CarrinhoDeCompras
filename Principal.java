76206df07ee609285d9fa598f1b7c07d2aa72a06
GabrielRdrigues <gabrielrodriguesmarquesvalim@gmail.com>
2025-01-15 21:27:37 -0300

    chaves formatacao

--- Principal.java
+++ Principal.java
@@ -2,24 +2,22 @@
 import java.util.Scanner;
 public class Principal
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         EstoqueProdutos estoque = new EstoqueProdutos();
-        //estoque.adicionaProduto(new Monitor(1000,10));
-        //estoque.adicionaProduto(new Telefone(3000,5));
+        CarrinhoCompra carrinho = new CarrinhoCompra(estoque);
         estoque.adicionaProduto(new Eletrodomestico("geladeira", 5000,100, "A+", 54, "Bivolt"));
         estoque.adicionaProduto(new Eletrodomestico("tv", 1000,10, "A++", 20, "110"));
         estoque.adicionaProduto(new Movel("sofa", 2000, 10, "Madeira", 2, 1.50, 1.25));
         estoque.adicionaProduto(new Eletronico("celular",2000,10,true,false));
 
-        // Começando a fazer o "menu"
+        
         Scanner ler = new Scanner(System.in); // o scanf do java é essa classe Scanner
         int opcao=0;
 
         System.out.println("Bem vindo as lojas Nuno Eletromóveis\n");
 
-        CarrinhoCompra carrinho = new CarrinhoCompra(estoque);
         while(opcao!=5)
-
         {
             System.out.println("Selecione uma das opções abaixo:");
             System.out.println("1-Imprimir catálogo\n2-Inserir item no carrinho\n3-Remover item do carrinho\n4-Finalizar Compra\n5-Sair");
@@ -27,7 +25,8 @@ public class Principal
             ler.nextLine(); // Se não colocar isso, o buffer fica parado no \n quando a gente digita a opção e
             // dá ruim na linha 29 (quando tentamos ler uma string)
 
-            switch(opcao) {
+            switch(opcao) 
+            {
                 case 1:
                     // Imprimir catálogo
                     estoque.imprimeCatalogo();
@@ -59,19 +58,24 @@ public class Principal
                     int metodoPagamento = ler.nextInt();
                     ler.nextLine(); // Limpa o buffer
                 
-                    if (metodoPagamento >= 1 && metodoPagamento <= 4) {
+                    if (metodoPagamento >= 1 && metodoPagamento <= 4) 
+                    {
 
                         carrinho.finalizaCompra(metodoPagamento); 
                         carrinho.limpar_carrinho();
                         System.out.println("\nDeseja voltar a comprar? (S/N)");
                         if (ler.next().charAt(0) == 'S')
+                        {
                             break;
-                            
-                        else {
+                        }
+                        else 
+                        {
                             System.out.println("Obrigado por comprar na Nuno Eletromóveis!");
                             opcao = 5;
                         }
-                    } else {
+                    } 
+                    else 
+                    {
                         System.out.println("Opção inválida. Compra cancelada.");
                     }
                     break;
